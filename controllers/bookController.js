f0d04f4993f6fa92c7795974c7ace7c8a92772a2
Faiyaz syed <faiyazahamedsyedgnt@gmail.com>
2026-07-20 12:36:08 +0530

    initial code

---
+++ controllers/bookController.js
@@ -0,0 +1,57 @@
+import Book from "../models/Book.js";
+
+// Get all books
+export const getBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Add a book
+export const addBook = async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    res.status(201).json(book);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Update a book
+export const updateBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+          returnDocument: "after",
+      }
+    );
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete a book
+export const deleteBook = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
