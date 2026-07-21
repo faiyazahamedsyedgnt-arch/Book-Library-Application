00759cde245ce52c126e073dda0fe6fcf2a7cc71
Faiyaz syed <faiyazahamedsyedgnt@gmail.com>
2026-07-21 10:10:32 +0530

    initial code

---
+++ models/book.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
